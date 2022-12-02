b9af66edb9d1ad3132e950c927ccb1de9080bc42
Sergey <mrmozolevsky@gmail.com>
2022-12-02 22:30:39 +0100

    MODULE-3:TASK-3: Limit size stream

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,19 +1,51 @@
-const url = require('url');
-const http = require('http');
-const path = require('path');
+const url = require('node:url');
+const http = require('node:http');
+const path = require('node:path');
+const fs = require('node:fs');
+const { access } = require('node:fs/promises');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
+server.on('request', async (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  // Temp file path just for tests
+  // const filepath = path.join(__dirname, 'test', 'fixtures', pathname);
+
   switch (req.method) {
     case 'GET':
 
+    if (pathname.includes('/')) {
+      res.statusCode = 400;
+      res.end('Nested paths are not supported');
       break;
+    }
+
+    try {
+      await access(filepath)
+    } catch ({name, message}) {
+      res.statusCode = 404;
+      res.end(`File by path ${pathname} does not exist. Error: ${name}`)
+    }
+
+    const stream = fs.createReadStream(filepath);
+
+    stream.on('error', ({name, message}) => {
+      res.statusCode = 500;
+      res.end(`Error: ${name}: ${message}. :()`)
+    })
+
+    res.on('close', () => {
+      console.log('network died')
+      stream.destroy();
+    })
+
+    res.statusCode = 200;
+    stream.pipe(res);
+    break;
 
     default:
       res.statusCode = 501;
