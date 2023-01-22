a4bcccf4c378a9aa81a6d32871c4c136f2d9aafa
Sergey <mrmozolevsky@gmail.com>
2023-01-22 21:47:02 +0100

    MODULE-6: TASK-1-2

--- 06-mongodb-mongoose/02-rest-api/libs/connection.js
+++ 06-mongodb-mongoose/02-rest-api/libs/connection.js
@@ -4,4 +4,10 @@ const config = require('../config');
 
 mongoose.plugin(beautifyUnique);
 
-module.exports = mongoose.createConnection(config.mongodb.uri);
+module.exports = mongoose.createConnection(config.mongodb.uri, err => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log('Mongo DB is connected')
+    }
+});
